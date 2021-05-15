4f7921047c032871f74a79cbb47ec678a271bb29
dunnousername <12957182+dunnousername@users.noreply.github.com>
2021-05-15 14:36:19 -0400

    stack grows downwards!

--- src/arch/x86/boot/stivale2.c
+++ src/arch/x86/boot/stivale2.c
@@ -4,7 +4,7 @@ struct stivale2_header_tag_terminal _stivale2_tag0;
 
 struct stivale2_header __attribute__(( section(".stivale2hdr") )) _stivale2_header = {
     0,
-    0x70000,
+    0x70000 + 32768,
     0,
     &_stivale2_tag0
 };
