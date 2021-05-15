d9570c834105c68ed23f05649e32464e85c06287
dunnousername <12957182+dunnousername@users.noreply.github.com>
2021-05-15 14:37:59 -0400

    use bss as stack instead

--- src/arch/x86/boot/stivale2.c
+++ src/arch/x86/boot/stivale2.c
@@ -1,10 +1,13 @@
 #include "../../../stivale/stivale2.h"
+#include <stdint.h>
 
 struct stivale2_header_tag_terminal _stivale2_tag0;
 
+static uint8_t stack[8192];
+
 struct stivale2_header __attribute__(( section(".stivale2hdr") )) _stivale2_header = {
     0,
-    0x70000 + 32768,
+    (uintptr_t)stack + sizeof(stack),
     0,
     &_stivale2_tag0
 };
