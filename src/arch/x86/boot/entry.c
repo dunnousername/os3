8f48b7abc4c3d2261b3e993d4333700a8c8394fb
dunnousername <12957182+dunnousername@users.noreply.github.com>
2021-05-15 14:28:21 -0400

    add boilerplate for stivale parsing

--- src/arch/x86/boot/entry.c
+++ src/arch/x86/boot/entry.c
@@ -16,20 +16,43 @@ static struct simpletty_info_t simpletty_info = {
     24
 };
 
-void simpletty_write(tty_driver_t *tty, char *s) {
+static void simpletty_write(tty_driver_t *tty, char *s) {
     while (*s) {
         *video++ = *s++;
         *video++ = 0x0F;
     }
 }
 
-void simpletty_clear(tty_driver_t *tty) {
+static void simpletty_clear(tty_driver_t *tty) {
 
+}
+
+struct stivale2_tag *parse_stivale(struct stivale2_tag *tag) {
+    if (!tag) {
+        return (void *) 0;
+    }
+
+    switch (tag->identifier) {
+        default:
+            break;
+    }
+
+    if (tag->next) {
+        return (void *) tag->next;
+    }
+
+    return (void *) 0;
 }
 
 void _cstart(struct stivale2_struct *stivale2_info) {
     chosen_tty.write = simpletty_write;
     chosen_tty.clear = simpletty_clear;
     chosen_tty.extra = &simpletty_info;
+
+    if (stivale2_info->tags) {
+        struct stivale2_tag *current = (void *) stivale2_info->tags;
+        while (current = parse_stivale(current));
+    }
+
     kernel_main();
 }
